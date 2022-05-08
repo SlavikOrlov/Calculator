ab71ecfd6793dfa27f9725c3f0086286e309297e
Slava Orlov <slavaorlov@192.168.0.4>
2022-05-09 00:24:12 +0300

    first commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Slava Orlov on 09.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
